d17b0449c074af8576a8ea34aa8dce57de898374
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-18 17:17:25 -0300

    Secret number modifications

---
+++ desafios_alura/app2.js
@@ -0,0 +1,26 @@
+let dia = prompt("Qual o dia da semana?");
+if(dia == "Sábado" || dia == "Domingo"){
+    alert("Bom fim de semana!");
+} else{
+    alert("Boa semana!");
+}
+
+let numero = prompt("Digite um número");
+if(numero >= 0){
+    alert("O número é positivo!");
+} else{
+    alert("O numero é negativo!");
+}
+
+let pontos = 0;
+if(pontos >= 100){
+    alert("Parabéns, você venceu!");
+} else{
+    alert("Tente novamente para ganhar.");
+}
+
+let saldo = 0;
+alert(`Seu saldo é R$ ${saldo}`);
+
+let nome = prompt("Qual o seu nome?");
+alert(`Seja bem-vindo(a) ${nome}!`);
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,23 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 10");
+let numeroSecreto = 2;
+console.log(`O numero secreto é ${numeroSecreto}`);
+let chute;
 
-if (chute == numeroSecreto){
-    console.log("Isso aí! Você descobriu o número secreto (5)")
+//enquanto chute não for igual ao numeroSecreto
+while (chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 10");
+    console.log("O chute foi " + chute);
+    console.log(`Resultado da comparação ${numeroSecreto == chute}`)
+
+    // se o chute for igual ao nummero secreto
+    if (chute == numeroSecreto){
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`)
+    }else{
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
+
