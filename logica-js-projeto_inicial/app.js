7266c7b0703cb346305c997dea9aefbc33e4634e
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-21 12:38:43 -0300

    tries to the guessing

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,6 +2,7 @@ alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 2;
 console.log(`O numero secreto é ${numeroSecreto}`);
 let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao numeroSecreto
 while (chute != numeroSecreto){
@@ -11,13 +12,15 @@ while (chute != numeroSecreto){
 
     // se o chute for igual ao nummero secreto
     if (chute == numeroSecreto){
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`)
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`)
     }else{
         if (chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
 
