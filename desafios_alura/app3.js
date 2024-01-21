bf091b4ff93884caa7175cc0284629b86f748a10
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-01-21 12:55:51 -0300

    new challenge and lesson

---
+++ desafios_alura/app3.js
@@ -0,0 +1,28 @@
+let i = 1;
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+
+
+i--;
+
+
+while (i >= 0){
+    console.log(i);
+    i--;
+}
+
+
+let f = prompt("Digite um numero");
+while (f >= 0){
+    console.log(f);
+    f--;
+}
+
+let j = prompt("Digite um numero");
+i = 0;
+while (i <= j){
+    console.log(i);
+    i--;
+}
