1b69a9cc1fd27f71df88bbe03ad187d3f3d9d690
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-02-06 19:20:56 -0300

    functions

---
+++ desafios_alura/app6.js
@@ -0,0 +1,22 @@
+function ola(){
+    console.log("Olá, mundo!");
+}
+function loa2(nome){
+    console.log(`Olá, ${nome}!`);
+}
+function dobro(numero){
+    return numero * 2;
+}
+function media(n1, n2, n3){
+    return (n1+n2+n3)/3;
+}
+function maior(a1, a2){
+    if (a1 < a2){
+        return a2;
+    }else{
+        return a1;
+    }
+}
+function quadrado(x){
+    return x*x;
+}
