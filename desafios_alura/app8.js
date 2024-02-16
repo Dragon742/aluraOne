61c95622299a360311a8c55ff8827ff3863db6a2
Lucas Vinicius Sales Dantas <lucasviniciussd2@gmail.com>
2024-02-16 17:40:20 -0300

    Update

---
+++ desafios_alura/app8.js
@@ -0,0 +1,12 @@
+let listaGenerica = [];
+
+let linguagensDeProgramação = ["JavaScript", "C", "C++", "Kotlin", "Python"];
+
+linguagensDeProgramação.push("Java", "Ruby", "GoLang");
+
+let lista = ["Nick", "Yuri", "Maria"];
+console.log(lista[0]);
+
+console.log(lista[1]);
+
+console.log(lista[2]);
